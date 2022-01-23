43ff7f68dcadf855210c725317edcff7efda431f
alya-ouadday <alya.benouadday@gmail.com>
2022-01-23 11:34:04 +0100

    desambiguate bis (exercice 1 sous forme de fonctions) le dictionnaire structures Le chunkParser pour la question 2 (généralisé) Le PDF avec les questions

--- TP1/chunkParser.py
+++ TP1/chunkParser.py
@@ -3,13 +3,14 @@ import sys
 from nltk import pos_tag
 from nltk import RegexpParser
 
-def chunk_parse(text):
+def chunk_parse(text, patterns):
+
     print(text)
     text = text.split()
     print("After Split:", text)
     tokens_tag = pos_tag(text)
     print("After Token:", tokens_tag)
-    patterns = """COMPOUND:{<DT>?<JJ>*<NN>}""" # le pattern qui spécifie la structure syntaxique à extraire
+    ##patterns = """COMPOUND:{<DT>?<JJ>*<NN>}""" # le pattern qui spécifie la structure syntaxique à extraire
 
     chunker = RegexpParser(patterns)
     print("After Regex:", chunker)
@@ -26,20 +27,30 @@ def chunk_parse(text):
     return matched_compound   # Si on veut tout écrire dans le fichier y compris
     #les élements qui ne sont pas "matchés" par le regexParser, il suffit de retourner tout le tree
 
-def chunk_parse_file(file_name_in):
+def chunk_parse_file(text_to_chunk_file, dict_file, structure):
 
-    f1 = open(file_name_in, "r")
-    lines = f1.read()
-    compounds = chunk_parse(lines)
-    f1.close()
-
-    f2 = open(sys.argv[1] + '.chk.nltk', "w")
-    for i in range(len(compounds)):
-        print("write compound : " + str(compounds[i]))
-        f2.write(str(compounds[i]))
-        f2.write("\n")
+    f2 = open(dict_file, "r")
+    lines = f2.read().split()
+    patterns = ''
+    for i in range(0, len(lines), 2):
+        if(lines[i].lower() == structure):
+            patterns = "COMPOUND:"+lines[i+1]
 
     f2.close()
 
+    f1 = open(text_to_chunk_file, "r")
+    lines = f1.read()
+    compounds = chunk_parse(lines, patterns)
+    f1.close()
+
+    f3 = open(sys.argv[1] + '.chk.nltk', "w")
+    f3.write(structure + ": \n")
+    for i in range(len(compounds)):
+        print("write compound : " + str(compounds[i]))
+        f3.write(str(compounds[i]))
+        f3.write("\n")
+
+    f3.close()
+
 if __name__ == '__main__':
-    chunk_parse_file(sys.argv[1])
+    chunk_parse_file(sys.argv[1], sys.argv[2], sys.argv[3].lower())
