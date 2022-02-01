acba8d2f58c168ada3cb3945115f064b07338cba
ashefd <lokchan.tshanon@gmail.com>
2022-02-01 18:07:55 +0100

    [Question 3] - Fonctionne sûrement. Pas sûre du résultat final

---
+++ TP1/use_ne_chunk.py
@@ -0,0 +1,35 @@
+import nltk
+import sys
+import io
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer
+
+def process_content(filename):
+    file = open(filename, 'w')
+    try:
+        for i in tokenized:
+            words = nltk.word_tokenize(i)
+            tagged = nltk.pos_tag(words)
+            namedEnt = nltk.ne_chunk(tagged, binary=True)
+            namedEnt.draw()
+    
+            with io.open(filename, 'w', encoding='utf8') as fout:
+                fout.write(str(namedEnt)+'\n\n')
+    
+    except Exception as e:
+        print(str(e))
+
+if __name__ == '__main__':
+
+    f = open(sys.argv[1], 'r') # wsj_0010_sample.txt
+    file = open(sys.argv[2], 'w') # wsj_0010_sample.txt.ne.nltk
+    lignes = f.read()
+
+    custom_sent_tokenizer = PunktSentenceTokenizer(lignes)
+
+    tokenized = custom_sent_tokenizer.tokenize(lignes)
+
+    process_content(sys.argv[2])
+
+
+# python use_ne_chunk.py wsj_0010_sample.txt wsj_0010_sample.txt.ne.nltk
