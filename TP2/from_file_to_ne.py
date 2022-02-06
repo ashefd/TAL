f5c3636560927d6e888ca49923d960f7739ef5f1
alya-ouadday <alya.benouadday@gmail.com>
2022-02-06 10:47:21 +0100

    question a et b , nb occurences et entitees nommées (photo png à mettre read me )

---
+++ TP2/from_file_to_ne.py
@@ -0,0 +1,47 @@
+import sys
+import csv
+
+def extract_ne(file_name):
+    f = open(file_name, "r")
+    words = f.read().split()
+    print(words)
+    dict_occurences = {}
+    dict_ne = {}
+    for w in words :
+        m = w.split("/")
+        key = m[0]
+        value = m[1]
+        if value != "O" and key in dict_occurences.keys():
+                dict_occurences[key] = dict_occurences[key] + 1
+        elif value != "O" :
+                dict_occurences[key] = 1
+                dict_ne[key] = value
+
+    return dict_occurences, dict_ne, len(words)
+
+def create_ne_rows(dict_occurences, dict_ne, size):
+    rows = [["Entite nommee",  "Type",  "Nombre d’occurrences", "Proportiondans le texte (%)"]]
+    for key in dict_occurences.keys() :
+        rows.append([key, dict_ne[key], dict_occurences[key], (dict_occurences[key]/size) * 100 ])
+    return rows
+
+def write_rows_in_file(file_name_out, rows):
+    with open(file_name_out+".csv", 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(rows)
+
+
+"""
+import csv
+row_list = [["SN", "Name", "Contribution"],
+             [1, "Linus Torvalds", "Linux Kernel"],
+             [2, "Tim Berners-Lee", "World Wide Web"],
+             [3, "Guido van Rossum", "Python Programming"]]
+with open('protagonist.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(row_list)
+    """
+if __name__ == '__main__':
+    dict_occurences, dict_ne, size = extract_ne(sys.argv[1])
+    rows = create_ne_rows(dict_occurences, dict_ne, size)
+    write_rows_in_file(sys.argv[2], rows)
