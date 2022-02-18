25b2d13114a485e35d59fe095dd5f7e2db0953ed
alyab <alya.benouadday@gmail.com>
2022-02-18 10:18:24 +0100

    ref without compounds

--- Projet/getOriginalCorpus.py
+++ Projet/getOriginalCorpus.py
@@ -3,6 +3,7 @@ import sys
 def extractSentences(file_name):
     line = ''
     line_without_compounds = ''
+    ref_without_compounds = ''
 
     file = open(file_name, 'r')
 
@@ -13,23 +14,34 @@ def extractSentences(file_name):
         if(len(x) == 0):
             line += '\n'
             line_without_compounds += '\n'
+            ref_without_compounds += '\n'
         # ajouter un elif(len(x) > 2): ajoute pas ds le talzau a ecrire 
         else:
+
             words = x[0:-1]
             line += ' '.join(words) + ' '
             if(len(x) == 2):
                 line_without_compounds += ' '.join(words) + ' '
+                ref_without_compounds += ' '.join(x) + '\n'
 
-    return line, line_without_compounds
+    return line, line_without_compounds, ref_without_compounds
 
 def writeLines(lines, file_out):
     file = open(file_out, 'w')
     file.write(lines)
 
 if __name__ == '__main__':
-    lines, line_without_compounds = extractSentences(sys.argv[1])
-    writeLines(lines, sys.argv[2])
-    without_compound = "out/" + sys.argv[1][:-5] + ".wc"+sys.argv[1][-5:]
-    print(without_compound)
-    writeLines(line_without_compounds, without_compound)
+    #1er arg : ref lima
+    #2eme : nom du file out pour le corpus (complet)
 
+    lines, line_without_compounds, ref_without_compounds = extractSentences(sys.argv[1])
+    writeLines(lines, sys.argv[2])
+    print("Corpus written in out/"+ sys.argv[2])
+
+    corpus_without_comp_out = sys.argv[2][:-4] + ".wc"+sys.argv[2][-4:]
+    ref_without_comp_out = "out/" + sys.argv[1][:-5] + ".wc" + sys.argv[1][-5:]
+
+    writeLines(line_without_compounds, corpus_without_comp_out)
+    print("Corpus without compounds written in " + sys.argv[2])
+    writeLines(ref_without_compounds, ref_without_comp_out)
+    print("Ref lima without compounds written in " + sys.argv[2])
